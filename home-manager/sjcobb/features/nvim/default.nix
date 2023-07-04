033d8e2d89fedca1628c4a449425c22cbfcbc69e
Samuel Cobb <sjcobb2003@gmail.com>
2023-07-04 16:29:55 +0100

    got astronvim working

--- home-manager/sjcobb/features/nvim/default.nix
+++ home-manager/sjcobb/features/nvim/default.nix
@@ -4,7 +4,15 @@
   imports = [
     ./neovide.nix
   ];
+  
   programs.neovim = {
     enable = true;
+  };
+
+  xdg.configFile."nvim".source = builtins.fetchGit {
+    url = "https://github.com/AstroNvim/AstroNvim.git";
+    ref = "main";
+    # rev = "87a05226b003c05369ca70ff7e7baf4910d0f8b1";
+    shallow = true;
   };
 }
