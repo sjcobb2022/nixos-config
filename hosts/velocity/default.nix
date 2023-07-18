6b64d45e78e4fc9b8d3ff04201e4209de95afbc5
System administrator <root@instance-20230717-1142>
2023-07-18 01:53:50 +0100

    added key

--- hosts/velocity/default.nix
+++ hosts/velocity/default.nix
@@ -22,6 +22,9 @@
   users.users.root.openssh.authorizedKeys.keys = [
     ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrk0T7LSF+LZ4mF3CdbVBp8Qcty9osSMkZZEm4rqy5tDqgHmd4SNr2KXDTENBAdo5Z4Ihdy5eXS/Q2OiBu9oWrVXpkEANb2LXIH3W3Af1q4U/72zTP+jCrpEj//2eULCeq7KcCiAmD6LekGMGqvUC5mHYw85LUWU5Jx+b5gXSQyzDozw35TRxGtjtEXs5fwAT0lXLwhzJWV4Su8pGaCd2NC6zZ38fcfISv8qjo8alKxxX+ZoUaXqGsm/ZedrmN3csjcxGuS7tekmstvoK/SEx7inwLWbpu6877Sj9AuGEG8hX0ASm4brkW4UsjOjNQjOmYjdOkVmHSE0P/2/modEv1 ssh-key-2023-07-17''
   ];
+  users.users.sjcobb.openssh.authorizedKeys.keys = [
+    ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrk0T7LSF+LZ4mF3CdbVBp8Qcty9osSMkZZEm4rqy5tDqgHmd4SNr2KXDTENBAdo5Z4Ihdy5eXS/Q2OiBu9oWrVXpkEANb2LXIH3W3Af1q4U/72zTP+jCrpEj//2eULCeq7KcCiAmD6LekGMGqvUC5mHYw85LUWU5Jx+b5gXSQyzDozw35TRxGtjtEXs5fwAT0lXLwhzJWV4Su8pGaCd2NC6zZ38fcfISv8qjo8alKxxX+ZoUaXqGsm/ZedrmN3csjcxGuS7tekmstvoK/SEx7inwLWbpu6877Sj9AuGEG8hX0ASm4brkW4UsjOjNQjOmYjdOkVmHSE0P/2/modEv1 ssh-key-2023-07-17''
+  ];
 
   time.timeZone = "Europe/London";
 
