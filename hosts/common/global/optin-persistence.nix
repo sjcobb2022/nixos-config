836d5508db32d4800931fdd57b501e9ccdbb2994
Samuel Cobb <sjcobb2003@gmail.com>
2023-08-05 23:06:32 +0100

    added loader options

--- hosts/common/global/optin-persistence.nix
+++ hosts/common/global/optin-persistence.nix
@@ -17,14 +17,14 @@
     };
   };
   
-  # system.activationScripts.persistent-dirs.text =
-  #   let
-  #     mkHomePersist = user: lib.optionalString user.createHome ''
-  #       mkdir -p /persist/${user.home}
-  #       chown ${user.name}:${user.group} /persist/${user.home}
-  #       chmod ${user.homeMode} /persist/${user.home}
-  #     '';
-  #     users = lib.attrValues config.users.users;
-  #   in
-  #   lib.concatLines (map mkHomePersist users);
+  system.activationScripts.persistent-dirs.text =
+    let
+      mkHomePersist = user: lib.optionalString user.createHome ''
+        mkdir -p /persist/${user.home}
+        chown ${user.name}:${user.group} /persist/${user.home}
+        chmod ${user.homeMode} /persist/${user.home}
+      '';
+      users = lib.attrValues config.users.users;
+    in
+    lib.concatLines (map mkHomePersist users);
 }
