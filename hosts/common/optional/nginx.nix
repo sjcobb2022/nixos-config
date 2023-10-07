e983d23a1c6028e54cca8ce0cd8444874a0130bc
Samuel Cobb <sjcobb2003@gmail.com>
2023-10-07 13:11:12 +0100

    test

--- hosts/common/optional/nginx.nix
+++ hosts/common/optional/nginx.nix
@@ -16,13 +16,9 @@
     '';
     appendHttpConfig = ''
 
-    upstream backend {
-      server G683083F1D51ED3-UJ2GE94JAJZME69W.adb.uk-london-1.oraclecloudapps.com;
-    }
-
     server {
-      location / {
-        proxy_pass http://backend;
+      location /db {
+        proxy_pass http://G683083F1D51ED3-UJ2GE94JAJZME69W.adb.uk-london-1.oraclecloudapps.com;
       }
     }
     '';
