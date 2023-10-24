0fa79e05317b7cc1a7c9a609723d665a3991c7b8
Samuel Cobb <sjcobb2003@gmail.com>
2023-10-24 13:12:15 +0100

    Updated wofi config

--- home-manager/common/features/desktop/common/wayland/wofi.nix
+++ home-manager/common/features/desktop/common/wayland/wofi.nix
@@ -4,24 +4,25 @@ let
     patches = (oa.patches or [ ]) ++ [
       # ./wofi-run-shell.patch # Fix for https://todo.sr.ht/~scoopta/wofi/174
     ];
-  });  
+  });
 
   pass = config.programs.password-store.package;
   passEnabled = config.programs.password-store.enable;
   pass-wofi = pkgs.pass-wofi.override { inherit pass; };
 in
 {
-  home.packages = [ wofi ] ++
-    (lib.optional passEnabled pass-wofi);
-
-  xdg.configFile."wofi/config".text = ''
-    image_size=48
-    columns=3
-    allow_images=true
-    insensitive=true
-
-    run-always_parse_args=true
-    run-cache_file=/dev/null
-    run-exec_search=true
-  '';
+  programs.wofi = {
+    enable = true;
+    settings = {
+      allow_markup = true;
+      display_generic = true;
+      image_size = 48;
+      columns = 3;
+      allow_images = true;
+      insensitive = true;
+      run-always_parse_args = true;
+      run-cache_file = "/dev/null";
+      run-exec_search = true;
+    };
+  };
 }
