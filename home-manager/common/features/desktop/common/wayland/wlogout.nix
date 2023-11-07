e108e5683b554ec95876964ae299d2ba66c579ab
Samuel Cobb <sjcobb2003@gmail.com>
2023-11-07 18:33:23 +0000

    wlogout config completed

--- home-manager/common/features/desktop/common/wayland/wlogout.nix
+++ home-manager/common/features/desktop/common/wayland/wlogout.nix
@@ -20,17 +20,15 @@
     # base0F: "#BB9AF7"
     style = let inherit (config.colorscheme) colors; in /* css */ ''
       * {
-              background-image: none;
+        background-image: none;
       }
       
       window {
-        background-color: rgba(23, 29, 35, 0.8);
+        background-color: alpha(#${colors.base00}, 0.8);
       }
       
       button {
         background-color: #${colors.base00};
-        border-style: solid;
-        border-width: 2px;
         background-repeat: no-repeat;
         background-position: center;
         background-size: 25%;
@@ -38,30 +36,35 @@
 
       button:focus, button:active, button:hover {
         background-color: #${colors.base01};
-        outline-style: none;
       }
 
       #lock {
+        opacity: 0.8;
         background-image: image(url("${../assets/wlogout/icons/lock.png}"), url("${pkgs.wlogout}/share/wlogout/icons/lock.png"));
       }
 
       #logout {
+        opacity: 0.8;
         background-image: image(url("${../assets/wlogout/icons/logout.png}"), url("${pkgs.wlogout}/share/wlogout/icons/logout.png"));
       }
 
       #suspend {
+        opacity: 0.8;
         background-image: image(url("${../assets/wlogout/icons/suspend.png}"), url("${pkgs.wlogout}/share/wlogout/icons/suspend.png"));
       }
 
       #hibernate {
+        opacity: 0.8;
         background-image: image(url("${../assets/wlogout/icons/hibernate.png}"), url("${pkgs.wlogout}/share/wlogout/icons/hibernate.png"));
       }
 
       #shutdown {
+        opacity: 0.8;
         background-image: image(url("${../assets/wlogout/icons/shutdown.png}"), url("${pkgs.wlogout}/share/wlogout/icons/shutdown.png"));
       }
 
       #reboot {
+        opacity: 0.8;
         background-image: image(url("${../assets/wlogout/icons/reboot.png}"), url("${pkgs.wlogout}/share/wlogout/icons/reboot.png"));
       }
     '';
