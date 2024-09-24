d106884bf85df5935dbcc3789ee7273a4db8a809
Samuel Cobb <sjcobb2003@gmail.com>
2024-09-24 23:10:38 +0100

    update flake.lock

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -11,7 +11,7 @@
   }: let
     forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"];
     pkgsFor = nixpkgs.legacyPackages;
-  in rec {
+  in {
     packages = forAllSystems (system: {
       default = pkgsFor.${system}.callPackage ./default.nix {};
     });
